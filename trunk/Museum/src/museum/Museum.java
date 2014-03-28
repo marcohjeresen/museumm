2eeeb53836c1693c5b1ad7dfca6717e684caaf34
markhjeresen@gmail.com <markhjeresen@gmail.com@05cb7641-8b1d-309c-9aac-cb1eefa0ed5e>
2014-03-28 10:47:18 +0000

    opret

---
+++ trunk/Museum/src/museum/Museum.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package museum;
+
+/**
+ *
+ * @author markh_000
+ */
+public class Museum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
